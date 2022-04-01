3b24e474320df575987fccf4b73916553ba7b5b0
Lucas Guilbert <lucas.guilbert15@gmail.com>
2022-04-01 19:40:37 +0200

    add camera shaking when base is damaged

--- Scenes/Maps/Game.gd
+++ Scenes/Maps/Game.gd
@@ -5,6 +5,7 @@ const _GameState: = preload("res://Scenes/Maps/GameState.gd")
 onready var towers_container: = get_node("Towers")
 onready var path_node: = get_node("Path")
 onready var ui_node: = get_node("UI")
+onready var camera: Camera2D = get_node("Camera2D")
 
 onready var state: _GameState = _GameState.new()
 
@@ -14,6 +15,12 @@ func _ready() -> void:
 	state.connect("wave_changed", self, "_on_wave_changed")
 	state.connect("wave_ended", self, "_on_wave_ended")
 	state.connect("health_changed", ui_node, "on_health_changed")
+
+# -------------------- SIGNALS --------------------
+
+func on_enemy_hit_base() -> void:
+	camera.shake(50)
+	state.on_enemy_hit_base()
 
 # -------------------- WAVE --------------------
 
@@ -31,7 +38,7 @@ func spawn_enemies() -> void:
 	for enemy in state.wave_data:
 		var new_enemy = GameData.get_enemy_from_id(enemy[0]).instance()
 		new_enemy.connect("die", state, "on_enemy_die")
-		new_enemy.connect("base_hit", state, "on_enemy_hit_base")
+		new_enemy.connect("base_hit", self, "on_enemy_hit_base")
 		path_node.add_child(new_enemy, true)
 		yield(get_tree().create_timer(enemy[1]), "timeout")
 
